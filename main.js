73733b1d8e1b527673837fc82821c322c36b4901
Erdem Ceylan <erdemxceylan@gmail.com>
2022-10-25 20:02:59 +0300

    interface usage

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+"use strict";
 //type-safe
-console.log("Typescript dosyasından merhaba");
+exports.__esModule = true;
+// console.log("Typescript dosyasından merhaba");
 function sayHello(name) {
     console.log("Hello ".concat(name));
 }
-sayHello("Seren");
+// sayHello("Seren");
 var sumFunction = function (a1, a2) {
     return a1 + a2;
 };
 var sumOfNumbers = sumFunction(5, 6);
-console.log(sumOfNumbers);
+// console.log(sumOfNumbers);
 var Greeter = /** @class */ (function () {
     function Greeter(name) {
         this.name = name;
@@ -23,4 +25,24 @@ var Greeter = /** @class */ (function () {
     return Greeter;
 }());
 var greeter = new Greeter("Halit");
-greeter.sayHi();
+// greeter.sayHi();
+var UserService = /** @class */ (function () {
+    function UserService() {
+        this.users = [];
+    }
+    UserService.prototype.add = function (user) {
+        this.users.push(user);
+    };
+    UserService.prototype.getUserNames = function () {
+        return this.users;
+    };
+    ;
+    return UserService;
+}());
+var userService = new UserService();
+userService.add({ name: 'Erdem', surname: 'Ceylan', age: 27 });
+console.log(userService.getUserNames());
+// userService class'ı oluşturalım
+// string[] dönüş tipi olarak getUserNames fonksiyonu oluşturalım
+// string name alan bir ekleme methodu
+// classin içinde bir alanda array ile tutalım. Ancak bu arraya dışardan erişim yasak!!
--- main.ts
+++ main.ts
@@ -1,37 +1,55 @@
 //type-safe
 
-console.log("Typescript dosyasından merhaba");
+import { User } from './user';
+
+// console.log("Typescript dosyasından merhaba");
 
 function sayHello(name: string) {
-	console.log(`Hello ${name}`);
+   console.log(`Hello ${name}`);
 }
 
-sayHello("Seren");
+// sayHello("Seren");
 
 let sumFunction = (a1: number, a2: number): number => {
-	return a1 + a2;
+   return a1 + a2;
 };
 
 let sumOfNumbers: number = sumFunction(5, 6);
-console.log(sumOfNumbers);
+// console.log(sumOfNumbers);
 
 class Greeter {
-	private name: string;
-	constructor(name: string) {
-		this.name = name;
-	}
-	sayHi() {
-		console.log(`Hello ${this.name}`);
-		this.sayWhatsUp();
-	}
+   private name: string;
+   constructor(name: string) {
+      this.name = name;
+   }
+   sayHi() {
+      console.log(`Hello ${this.name}`);
+      this.sayWhatsUp();
+   }
 
-	private sayWhatsUp() {
-		console.log("What's up?");
-	}
+   private sayWhatsUp() {
+      console.log("What's up?");
+   }
 }
 
 let greeter = new Greeter("Halit");
-greeter.sayHi();
+// greeter.sayHi();
+
+class UserService {
+   private users: User[] = [];
+
+   add(user: User) {
+      this.users.push(user);
+   }
+
+   getUserNames(): User[] {
+      return this.users;
+   };
+}
+
+let userService = new UserService();
+userService.add({ name: 'Erdem', surname: 'Ceylan', age: 27 });
+console.log(userService.getUserNames());
 
 // userService class'ı oluşturalım
 // string[] dönüş tipi olarak getUserNames fonksiyonu oluşturalım
